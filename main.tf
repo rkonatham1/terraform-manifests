86825541e58834d2addcc35276488121fbd834ae
RamanaReddy Konatham <azdevopsknowledgehub@gmail.com>
2024-04-07 17:52:06 +0530

    added terraform file

---
+++ main.tf
@@ -0,0 +1,154 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = hashicorpazurerm
+      version = 3.10.0
+    }
+  }
+}
+
+provider azurerm {
+  subscription_id = 8c77ea9p-caa2-4d5f-a7f5-cec6d95d164c
+  tenant_id = a11bc5a9-502f-4f29-92b8-9742641e0c69
+  client_id = 6fa7b39f-2009-4850-be23-ce897c3eba77
+  client_secret = JsK8Q~7SsL3B9242SHCuArkzLSlc3ZkkC1XIdaFM
+  features {}  
+}
+
+locals {
+  resource_group_name=app-grp
+  location=North Europe
+  virtual_network={
+    name=app-network
+    address_space=10.0.0.016
+  }
+
+  subnets=[
+    {
+      name=subnetA
+      address_prefix=10.0.0.024
+    },
+    {
+      name=subnetB
+      address_prefix=10.0.1.024
+    }
+  ]
+}
+
+resource azurerm_resource_group appgrp {
+  name     = local.resource_group_name
+  location = local.location  
+}
+
+resource azurerm_virtual_network appnetwork {
+  name                = local.virtual_network.name
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  address_space       = [local.virtual_network.address_space]  
+  
+   depends_on = [
+     azurerm_resource_group.appgrp
+   ]
+  }
+
+
+  resource azurerm_subnet subnetA {
+  name                 = local.subnets[0].name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = local.virtual_network.name
+  address_prefixes     = [local.subnets[0].address_prefix]
+  depends_on = [
+    azurerm_virtual_network.appnetwork
+  ]
+}
+
+resource azurerm_subnet subnetB {
+  name                 = local.subnets[1].name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = local.virtual_network.name
+  address_prefixes     = [local.subnets[1].address_prefix]
+  depends_on = [
+    azurerm_virtual_network.appnetwork
+  ]
+}
+
+resource azurerm_network_interface appinterface {
+  name                = appinterface
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                          = internal
+    subnet_id                     = azurerm_subnet.subnetA.id
+    private_ip_address_allocation = Dynamic
+    public_ip_address_id = azurerm_public_ip.appip.id
+  }
+  depends_on = [
+    azurerm_subnet.subnetA
+  ]
+}
+
+resource azurerm_public_ip appip {
+  name                = app-ip
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  allocation_method   = Static
+ depends_on = [
+   azurerm_resource_group.appgrp
+ ]
+}
+
+resource azurerm_network_security_group appnsg {
+  name                = app-nsg
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  security_rule {
+    name                       = AllowRDP
+    priority                   = 300
+    direction                  = Inbound
+    access                     = Allow
+    protocol                   = Tcp
+    source_port_range          = 
+    destination_port_range     = 3389
+    source_address_prefix      = 
+    destination_address_prefix = 
+  }
+
+  depends_on = [
+    azurerm_resource_group.appgrp
+  ]
+}
+
+resource azurerm_subnet_network_security_group_association appnsglink {
+  subnet_id                 = azurerm_subnet.subnetA.id
+  network_security_group_id = azurerm_network_security_group.appnsg.id
+}
+
+resource azurerm_windows_virtual_machine appvm {
+  name                = appvm
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  size                = Standard_D2S_v3
+  admin_username      = adminuser
+  admin_password      = Azure@123
+  network_interface_ids = [
+    azurerm_network_interface.appinterface.id,
+  ]
+
+  os_disk {
+    caching              = ReadWrite
+    storage_account_type = Standard_LRS
+  }
+
+  source_image_reference {
+    publisher = MicrosoftWindowsServer
+    offer     = WindowsServer
+    sku       = 2019-Datacenter
+    version   = latest
+  }
+  depends_on = [
+    azurerm_network_interface.appinterface,
+    azurerm_resource_group.appgrp
+  ]
+}
